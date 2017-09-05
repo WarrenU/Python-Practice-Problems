b85a263d65e38d5b983a29502bb807128bce9adb
Warren Urbina <warren.urbina@gmail.com>
2017-09-04 20:24:02 -0700

    project euler problem 1 sum of multiples of 3 and 5

---
+++ 2-Sum-Multiples-3-5.py
@@ -0,0 +1,24 @@
+def sum_multiples(threshold, *args):
+    """
+    Takes a threshold of numbers to go below. For example, 10
+      Then it will sum the numbers that are multiples of *args
+      you pass.
+    :param threshold: integer
+    :param *args: integer (multiples you want to pass) 3, 5
+    """
+    sum = 0
+    summed_multiples = []
+    for x in range(threshold):
+        for a in args:
+            if (x%a) == 0 and x not in summed_multiples:
+                sum += x
+                summed_multiples.append(x)
+    return sum
+
+def main():
+    print(sum_multiples(10,3,5))  # returns 23
+    print(sum_multiples(1000,3,5))  # returns 233168
+
+
+if __name__ == "__main__":
+    main()
